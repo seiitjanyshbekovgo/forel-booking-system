0b751d49d88b1af90e615fdf872388508f3ff585
suinaly seiit <suinalyseiit@MacBook-Air-suinaly.local>
2026-07-13 22:26:40 +0600

    test lazygit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Admin from "./pages/Admin";
 import AdminLogin from "./pages/AdminLogin";
 
-import Home from "./pages/Home";
-import Tables from "./pages/Tables";
-import TableDetails from "./pages/TableDetails";
-import Contact from "./pages/Contact";
-import Branches from "./pages/Branches";
 import BookingStatus from "./pages/BookingStatus";
+import Branches from "./pages/Branches";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import TableDetails from "./pages/TableDetails";
+import Tables from "./pages/Tables";
 
 import Footer from "./components/Footer";
 import SelectBranch from "./pages/SelectBranch";
 
 function AppContent() {
   const location = useLocation();
-
+  // test 
   const hideFooter =
     location.pathname === "/admin" || location.pathname === "/admin-login";
 
